38cbff3eb3c7f5f4b990cdd7aecbb1be9ecbd964
erdogant <erdogant@gmail.com>
2022-03-11 12:39:59 +0100

    update setup file

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
      description="pypickle is to save and load variables in/from pickle files",
      long_description=long_description,
      long_description_content_type="text/markdown",
-     url="https://github.com/erdogant/pypickle",
+     url="https://erdogant.github.io/pypickle",
 	 download_url = 'https://github.com/erdogant/pypickle/archive/'+new_version+'.tar.gz',
      packages=setuptools.find_packages(), # Searches throughout all dirs for files to include
      include_package_data=True, # Must be true to include files depicted in MANIFEST.in
