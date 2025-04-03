705e89fb4d33b3c4009339ef9814efd0a960dd0f
erdogant <erdogant@gmail.com>
2025-04-03 11:13:33 +0200

    new version

--- pypickle/__init__.py
+++ pypickle/__init__.py
@@ -7,7 +7,7 @@ from pypickle.pypickle import (
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '1.1.1'
+__version__ = '1.1.3'
 
 # module level doc-string
 __doc__ = """
