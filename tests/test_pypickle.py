d9bcfc90933ba0384c04f1874257d80340d5c368
erdogant <erdogant@gmail.com>
2025-04-08 15:48:33 +0200

    add logger to unit test

--- tests/test_pypickle.py
+++ tests/test_pypickle.py
@@ -1,5 +1,18 @@
 import pypickle
 
+def test_logger():
+    pypickle.check_logger(verbose=0)
+    pypickle.check_logger(verbose=20)
+    pypickle.check_logger(verbose=30)
+    pypickle.check_logger(verbose=40)
+    pypickle.check_logger(verbose=50)
+
+    pypickle.check_logger(verbose='silent')
+    pypickle.check_logger(verbose='debug')
+    pypickle.check_logger(verbose='info')
+    pypickle.check_logger(verbose='warning')
+    pypickle.check_logger(verbose='error')
+
 def test_save():
     data = [1,2,3,4,5]
     assert pypickle.save('test.pkl', data, overwrite=True)
