10efad75f713e20d4d35a8afa8bd3be987ce3714
erdogant <erdogant@gmail.com>
2025-05-22 19:58:27 +0200

    update exmaple

--- pypickle/examples.py
+++ pypickle/examples.py
@@ -3,21 +3,6 @@
 # print(dir(pypickle))
 # print(pypickle.__version__)
 
-# %%
-# Saving and loading
-import bnlearn as bn
-
-# Load example mixed dataset
-df = bn.import_example(data='sprinkler')
-
-# Structure learning
-model = bn.structure_learning.fit(df)
-model = bn.independence_test(model, df, test='chi_square', prune=True)
-model = bn.parameter_learning.fit(model, df)
-
-filepath = 'model_bnlearn.pkl'
-bn.save(model, filepath)
-bn.load(filepath)
 
 
 # %% Verbose test
